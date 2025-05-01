b2f4b9daa3cf0e376a1f9e16ff7944bec9d58da7
Kirill <mail@mail.com>
2025-05-01 19:08:12 +0300

    начало работы (с прицелом)

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from wrap_mENdRU.actions import set_size_percent
 
 wrap.world.create_world(1000, 900)
 wrap.add_sprite_dir('sprites')
+u= wrap.sprite.add('zastavka', 500, 500, 'new_okno',visible=False)
+wrap.sprite.set_size_percent(u,250,250)
 z = wrap.sprite.add('zastavka', 500, 500, 'заставка')
+
 
 # k=wrap.sprite.add('zastavka',500,400,'img')
 # wrap.sprite.set_size_percent(k,40,30)
@@ -12,22 +15,24 @@ t = wrap.sprite.add_text(' МИССИЯ ', 500, 400, font_size=50, italic=True, 
 
 
 # wrap.sprite.set_size_percent(t,300,300)
-@wrap.on_mouse_up(wrap.BUTTON_LEFT)
+@wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def poxod_na_missiyo(pos_x, pos_y):
-    t_p = wrap.sprite.is_collide_point(t, pos_x, pos_y)
-    if t_p == True:
-        wrap.sprite.hide(t)
-        wrap.sprite.set_costume(z, 'new_okno')
-
-
-@wrap.on_mouse_up(wrap.BUTTON_LEFT)
-def perehod_pric(pos_x, pos_y):
     c=wrap.sprite.get_costume(z)
 
-    if c=='new_okno' and :
-        wrap.sprite.set_costume(z, 'игра')
+    if wrap.sprite.exist(t)==True:
+        t_p = wrap.sprite.is_collide_point(t, pos_x, pos_y)
+        if t_p == True:
+            wrap.sprite.remove(t)
+            wrap.sprite.set_costume(z, 'new_okno')
 
-    #     wrap.sprite,set_size_percent(k,35,25)
-    #     time.sleep(0.1)
-    #     wrap.sprite,set_size_percent(k,40,30)
-    #     print(5)
+    if c=='new_okno':
+        wrap.sprite.set_costume(z, 'игра')
+        wrap.sprite.show(u)
+
+
+
+@wrap.on_mouse_up(wrap.BUTTON_LEFT)
+def spusk_pric(pos_x,pos_y):
+    if wrap.sprite.exist(t)==False:
+        wrap.sprite.set_costume(z,'new_okno')
+
